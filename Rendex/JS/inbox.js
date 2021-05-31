a309cef610a00af01e830cc38a22949b179f9bda
Patrick Tannoury <tannouryp@gmail.com>
2021-05-31 10:06:32 +0200

    inbox fixes

--- Rendex/JS/inbox.js
+++ Rendex/JS/inbox.js
@@ -1,14 +1,29 @@
 var insertWindow = document.getElementById('insert-window');
 var windowButton = document.getElementById('insert-button-chat');
+var chatOptionsButton = document.getElementById('conversation-options');
+var chatOptionsWindow = document.getElementById('conversation-options-window');
 
+/*
 function displayInsertWindow(){
     insertWindow.style.display = "flex";
 }
-window-addEventListener('click', function(e){
-    if(document.getElementById('insert-button-chat').contains(e.target)){
-        //clicked in box
+function openChatOptions(){
+    chatOptionsWindow.style.display = "flex";
+} */
+
+window.addEventListener('click', function(e){
+    var select = e.target;
+    var a = windowButton;
+    var b = chatOptionsButton;
+
+    if(select == a || select == insertWindow){
+        insertWindow.style.display = "flex";
     }
-    else{
+    else if(select == b || select == chatOptionsWindow){
+        chatOptionsWindow.style.display = "flex";
+    }
+    else if (select !== a || select !== insertWindow){
         insertWindow.style.display = "none";
+        chatOptionsWindow.style.display = "none";
     }
-})
+});
