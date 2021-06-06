d2399a2ea1dc0d9ca4453369b500353018378170
Patrick Tannoury <tannouryp@gmail.com>
2021-06-06 12:40:31 +0200

    'jagger' fix

--- Rendex/JS/createaccount.js
+++ Rendex/JS/createaccount.js
@@ -1,4 +1,4 @@
-$.getJSON("https://skatteverket.entryscape.net/rowstore/dataset/b4de7df7-63c0-4e7e-bb59-1f156a591763/swagger", function(personId){
+$.getJSON("https://skatteverket.entryscape.net/rowstore/dataset/b4de7df7-63c0-4e7e-bb59-1f156a591763", function(personId){
     console.log(personId);
 })
 /********************************PERSONNUMMER************************************ */
