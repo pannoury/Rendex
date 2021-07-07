4c96e2fc485fa3730a215ade2442f2330a4186e2
Rendex-sweden <84630659+Rendex-sweden@users.noreply.github.com>
2021-07-08 00:06:49 +0200

    Wednesday Fix

--- public_html/JS/cityarray.js
+++ public_html/JS/cityarray.js
@@ -1,7 +1,7 @@
-const blekingeCityArray = ["Karlshamn", "Karlskrona", "Olofström", "Ronneby", "Sölvesborg"];
-const dalarnaCityArray = ["Avesta", "Borlänge", "Falun", "Gagnef", "Hedemora", "Leksand", "Ludvika", "Malung-Sälen", "Mora", "Orsa", "Rättvik", "Smedjebacken", "Säter", "Vansbro", "Älvdalen"];
-const gotlandCityArray = ["Hela Gotland"];
-const gävleborgCityArray = ["Bollnäs", "Gävle", "Hofors", "Hudiksval",  "Ljusdal", "Nordanstig", "Ockelbo", "Ovanåker", "Sandviken", "Söderhamn"];
-const hallandCityArray = ["Falkenberg", "Halmstad", "Hylte", "Hudiksval", "Laholm", "Varberg"];
-const jämtlandCityArray = ["Berg", "Bräcke", "Härjedalen", "Krokom", "Ragunda", "Strömsund", "Åre", "Östersund"];
-const jönköpingCityArray = ["Aneby", "Eksjö", "Gislaved",  "Gnosjö", "Habo", "Jönköping", "Mullsjö", "Nässjö", "Sävsjö", "Tranås", "Vaggeryd", "Vetlanda",  "Värnamo"];
+var blekingeCityArray = ["Karlshamn", "Karlskrona", "Olofström", "Ronneby", "Sölvesborg"];
+var dalarnaCityArray = ["Avesta", "Borlänge", "Falun", "Gagnef", "Hedemora", "Leksand", "Ludvika", "Malung-Sälen", "Mora", "Orsa", "Rättvik", "Smedjebacken", "Säter", "Vansbro", "Älvdalen"];
+var gotlandCityArray = ["Hela Gotland"];
+var gävleborgCityArray = ["Bollnäs", "Gävle", "Hofors", "Hudiksval",  "Ljusdal", "Nordanstig", "Ockelbo", "Ovanåker", "Sandviken", "Söderhamn"];
+var hallandCityArray = ["Falkenberg", "Halmstad", "Hylte", "Hudiksval", "Laholm", "Varberg"];
+var jämtlandCityArray = ["Berg", "Bräcke", "Härjedalen", "Krokom", "Ragunda", "Strömsund", "Åre", "Östersund"];
+var jönköpingCityArray = ["Aneby", "Eksjö", "Gislaved",  "Gnosjö", "Habo", "Jönköping", "Mullsjö", "Nässjö", "Sävsjö", "Tranås", "Vaggeryd", "Vetlanda",  "Värnamo"];
